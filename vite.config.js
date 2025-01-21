8d6e0fdffde76b9a2ffc4895164cf851faedaf58
Antônio César <antoniocesar1694@gmail.com>
2025-01-21 14:54:27 -0300

    Added jetstream

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import html from '@rollup/plugin-html';
-import { glob } from 'glob';
-
-/**
- * Get Files from a directory
- * @param {string} query
- * @returns array
- */
-function GetFilesArray(query) {
-  return glob.sync(query);
-}
-/**
- * Js Files
- */
-// Page JS Files
-const pageJsFiles = GetFilesArray('resources/assets/js/*.js');
-
-// Processing Vendor JS Files
-const vendorJsFiles = GetFilesArray('resources/assets/vendor/js/*.js');
-
-// Processing Libs JS Files
-const LibsJsFiles = GetFilesArray('resources/assets/vendor/libs/**/*.js');
-
-/**
- * Scss Files
- */
-// Processing Core, Themes & Pages Scss Files
-const CoreScssFiles = GetFilesArray('resources/assets/vendor/scss/**/!(_)*.scss');
-
-// Processing Libs Scss & Css Files
-const LibsScssFiles = GetFilesArray('resources/assets/vendor/libs/**/!(_)*.scss');
-const LibsCssFiles = GetFilesArray('resources/assets/vendor/libs/**/*.css');
-
-// Processing Fonts Scss Files
-const FontsScssFiles = GetFilesArray('resources/assets/vendor/fonts/!(_)*.scss');
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/assets/css/demo.css',
-        'resources/js/app.js',
-        ...pageJsFiles,
-        ...vendorJsFiles,
-        ...LibsJsFiles,
-        ...CoreScssFiles,
-        ...LibsScssFiles,
-        ...LibsCssFiles,
-        ...FontsScssFiles
-      ],
-      refresh: true
-    }),
-    html()
-  ]
+    plugins: [
+        laravel({
+            input: [
+              'resources/css/app.css',
+              'resources/js/app.js',
+              'resources/assets/vendor/libs/apex-charts/apex-charts.scss',
+              'resources/assets/vendor/libs/apex-charts/apexcharts.js',
+              'resources/assets/js/dashboards-analytics.js',
+              'resources/assets/vendor/fonts/boxicons.scss',
+              'resources/assets/vendor/scss/core.scss',
+              'resources/assets/vendor/scss/theme-default.scss',
+              'resources/assets/css/demo.css',
+              'resources/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.scss',
+              'resources/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js',
+              'resources/assets/vendor/js/helpers.js',
+              'resources/assets/js/config.js',
+              'resources/assets/vendor/libs/jquery/jquery.js',
+              'resources/assets/vendor/libs/popper/popper.js',
+              'resources/assets/vendor/js/bootstrap.js',
+              'resources/assets/vendor/js/menu.js',
+              'resources/assets/js/main.js',
+              'resources/assets/vendor/scss/pages/page-auth.scss'
+            ],
+            refresh: true,
+        }),
+    ],
 });
